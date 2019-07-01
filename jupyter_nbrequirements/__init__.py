bcb5385bdff742b1e14214cbf1caa1856c9d1aff
Marek Cermak <macermak@redhat.com>
2019-07-01 13:26:38 +0200

    Requirements config magic
    Signed-off-by: Marek Cermak <macermak@redhat.com>
    
    modified:   jupyter_nbrequirements/__init__.py

--- jupyter_nbrequirements/__init__.py
+++ jupyter_nbrequirements/__init__.py
@@ -23,6 +23,8 @@
 
 """Jupyter magic extension for managing notebook requirements."""
 
+import re
+import sys
 import json
 
 from pathlib import Path
@@ -32,6 +34,10 @@ from IPython.core.magic import magics_class
 from IPython.core.magic import Magics
 
 from jupyter_require import execute as executejs
+
+from thamos.config import config
+from thamos.config import workdir
+
 from thoth.python import Pipfile
 
 from .magic_parser import MagicParser
@@ -40,15 +46,52 @@ from .magic_parser import MagicParser
 _HERE = Path(__file__).parent
 
 
-def _requirements(args) -> str:
+def _requirements(args, params: dict = None, **kwargs) -> str:
     """Return script to be executed on `requirements` command."""
-    sub_command = f"_{args.script}" if args.script else ""
+    params = params or dict()
 
+    sub_command = f"_{args.script}" if args.script else ""
     with open(_HERE / f"static/requirements{sub_command}.js") as f:
         script = f.read()
     
-    return script
+    params.update(kwargs)
+    params["magic_args"] = json.dumps(args.__dict__, default=lambda s: repr(s))
 
+    return script, params
+
+
+def _requirements_config(args):
+    """Return script to be executed on `requirements` command."""
+    if not config.config_file_exists():
+        config.create_default_config()
+
+        script = f"console.log('Config file has been created:', {json.dumps(config.content)})"
+    else:
+        script = f"console.log('Config file already exists:', {json.dumps(config.content)})"
+
+    with workdir(config.CONFIG_NAME) as config_dir:
+        p = Path(config_dir, config.CONFIG_NAME)
+        content: str = p.read_text()
+
+        # TODO: Allow the user to edit the file directly in Jupyter
+        for arg, value in args.__dict__.items():
+            if value is not None and arg in config.content:
+                pattern = re.compile(f"{arg}: (.*)")
+                content = re.sub(pattern, f"{arg}: {value}", content, count=1)
+
+        # write with changes
+        p.write_text(content)
+    
+    config.load_config()
+
+    if args.to_json:
+        print(json.dumps(config.content, indent=4))
+    else:
+        print(
+            "\n".join(filter(lambda s: not s.strip().startswith("#"), content.splitlines()))
+        )
+
+    return script, {}
 
 @magics_class
 class RequirementsMagic(Magics):
@@ -107,12 +150,12 @@ class RequirementsMagic(Magics):
                 help="Whether to overwrite existing file."
             )
 
-            subparsers = parser.add_subparsers(dest="script")
+            subparsers = parser.add_subparsers(dest="command")
 
             # subcommand: lock
             parser_lock = subparsers.add_parser(
                 "lock",
-                help="Lock (pin down) dependencies."
+                description="Lock (pin down) dependencies."
             )
             parser_lock.add_argument(
                 "-b", "--backend",
@@ -126,34 +169,49 @@ class RequirementsMagic(Magics):
             # subcommand: config
             parser_config = subparsers.add_parser(
                 "config",
-                help="Generate Thoth config."
+                description="Generate Thoth config."
             )
-            parser_config.set_defaults(func=_requirements)
+            parser_config.add_argument(
+                "--host",
+                type=str,
+                help="A remote Thoth service to talk to."
+            )
+            parser_config.add_argument(
+                "--requirements-format",
+                type=str,
+                choices=["pipenv"],
+                help="Format of requirements file, currently supported is only Pipenv."
+            )
+            parser_config.add_argument(
+                "--tls-verify",
+                type=str,
+                choices=["false", "true"],
+                help="TLS verification for communication with remote Thoth instance."
+            )
+            parser_config.set_defaults(func=_requirements_config)
 
             # subcommand: install
             parser_install = subparsers.add_parser(
                 "install",
-                help="Install pinned down dependencies."
+                description="Install pinned down dependencies."
             )
             parser_install.set_defaults(func=_requirements)
 
             # subcommand: kernel
             parser_kernel = subparsers.add_parser(
                 "kernel",
-                help="Create new Jupyter kernel."
+                description="Create new Jupyter kernel."
             )
             parser_kernel.set_defaults(func=_requirements)
 
             opts = line.split()
+            args = parser.parse_args(opts)
 
             if any([opt in {"-h", "--help"} for opt in opts]):
                 # print help and return
                 return
 
-            args = parser.parse_args(opts)
-
-            script = args.func(args)
-            params["magic_args"] = json.dumps(args.__dict__, default=lambda s: repr(s))
+            script, params = args.func(args)
 
         return executejs(script, **params)
 

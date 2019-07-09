616c8a82c4ec83b0839967fc4d1e70b34d922754
Marek Cermak <macermak@redhat.com>
2019-07-09 08:53:59 +0200

    Setuptools
    Signed-off-by: Marek Cermak <macermak@redhat.com>
    
    new file:   ../Pipfile
    new file:   ../requirements.txt
    new file:   ../setup.py

---
+++ setup.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function
+
+from pathlib import Path
+from setuptools import setup
+from setuptools import find_packages
+
+
+HERE = Path(__file__).parent
+NAME = 'jupyter_nbrequirements'
+
+ABOUT = dict()
+exec(Path(HERE, NAME, '__about__.py').read_text(), ABOUT)
+
+README: str = Path(HERE, "README.md").read_text(encoding='utf-8')
+REQUIREMENTS: list = Path(HERE, 'requirements.txt').read_text().splitlines()
+
+setup(
+    name=ABOUT['__title__'],
+    version=ABOUT['__version__'],
+
+    author=ABOUT['__author__'],
+    author_email=ABOUT['__email__'],
+    url=ABOUT['__uri__'],
+
+    license=ABOUT['__license__'],
+
+    description=ABOUT['__summary__'],
+    long_description=README,
+    long_description_content_type='text/markdown',
+
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Framework :: IPython",
+        "Framework :: Jupyter",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: JavaScript",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Utilities"
+    ],
+
+    install_requires=REQUIREMENTS,
+
+    packages=find_packages(),
+    include_package_data=True,
+
+    package_data={
+        NAME: ["static/*.js"]
+    },
+
+    zip_safe=False
+)

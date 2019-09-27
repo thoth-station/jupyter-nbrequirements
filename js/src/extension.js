e03ee450dd6ae57d677c92d758b2f46e38d3ca6d
Marek Cermak <macermak@redhat.com>
2019-09-27 13:30:43 +0200

    Autoload the python extension
    Signed-off-by: Marek Cermak <macermak@redhat.com>
    
    modified:   src/extension.js
    modified:   ../jupyter_nbrequirements/static/extension.js

--- js/src/extension.js
+++ js/src/extension.js
@@ -11,6 +11,8 @@
  * @since  0.0.1
  */
 
+__extension__ = "jupyter_nbrequirements"
+
 // Some static assets may be required by the custom widget javascript. The base
 // url for the notebook is not known at build time and is therefore computed
 // dynamically.
@@ -33,5 +35,28 @@ if ( window.require ) {
 
 // Export the required load_ipython_extension
 module.exports = {
-    load_ipython_extension: function () { }
+    load_ipython_extension: function () {// Autoload
+        // wait for both the kernel and the jupyter-require extension to be loaded
+        window.require( [
+            "nbextensions/jupyter-require/extension"
+        ], () => {
+            const options = {
+                silent: true,
+                // if there is an error, let user try to manually
+                // load the extension himself and finish the extension
+                // loading anyway
+                stop_on_error: false,
+                store_history: false
+            }
+
+            setTimeout( () => {
+
+                console.info( "Loading extension `nbrequirements` into the notebook context." )
+
+                const cmd = "%load_ext " + __extension__
+                Jupyter.notebook.kernel.execute( cmd, {}, options )
+
+            }, 1000 )
+        } )
+    }
 };

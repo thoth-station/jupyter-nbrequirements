3ff587a0f4cdc070dfc1b4afc1cf614f7eb1266d
Marek Cermak <macermak@redhat.com>
2019-09-28 17:00:27 +0200

    Autoloading
    Signed-off-by: Marek Cermak <macermak@redhat.com>
    
    modified:   src/extension.js
    modified:   ../jupyter_nbrequirements/static/extension.js

--- js/src/extension.js
+++ js/src/extension.js
@@ -11,7 +11,7 @@
  * @since  0.0.1
  */
 
-__extension__ = "jupyter_nbrequirements"
+const __extension__ = "jupyter_nbrequirements"
 
 // Some static assets may be required by the custom widget javascript. The base
 // url for the notebook is not known at build time and is therefore computed
@@ -36,28 +36,29 @@ if ( window.require ) {
 // Export the required load_ipython_extension
 module.exports = {
     load_ipython_extension: function () {// Autoload
-        // TODO: There is still an issue on jupyter-require site which blocks this
-        //     // wait for both the kernel and the jupyter-require extension to be loaded
-        //     window.require( [
-        //         "nbextensions/jupyter-require/extension"
-        //     ], () => {
-        //         const options = {
-        //             silent: true,
-        //             // if there is an error, let user try to manually
-        //             // load the extension himself and finish the extension
-        //             // loading anyway
-        //             stop_on_error: false,
-        //             store_history: false
-        //         }
+        // wait for both the kernel and the jupyter-require extension to be loaded
+        window.require( [
+            "base/js/namespace",
+            "base/js/events"
+        ], ( Jupyter, events ) => {
+            const options = {
+                silent: false,
+                // if there is an error, let user try to manually
+                // load the extension himself and finish the extension
+                // loading anyway
+                stop_on_error: true,
+                store_history: false
+            }
 
-        //         setTimeout( () => {
+            // Wait for the required extension to be loaded
+            events.one( "extension_loaded.JupyterRequire", () => {
 
-        //             console.info( "Loading extension `nbrequirements` into the notebook context." )
+                console.info( "Loading magic commands: [ '%dep', '%requirements', '%kernel' ]" )
 
-        //             const cmd = "%load_ext " + __extension__
-        //             Jupyter.notebook.kernel.execute( cmd, {}, options )
+                const cmd = "%reload_ext " + __extension__
+                Jupyter.notebook.kernel.execute( cmd, {}, options )
 
-        //         }, 2000 )
-        //     } )
+            } )
+        } )
     }
 };
